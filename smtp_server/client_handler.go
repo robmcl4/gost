b7771e4740f9478f59d1ffdd82c7d5136a739ccc
Robert McLaughlin <robert@sparkk.us>
2015-09-06 09:42:59 -0400

    add method to get command from the client

--- smtp_server/client_handler.go
+++ smtp_server/client_handler.go
@@ -1,1 +1,78 @@
 package smtp_server
+
+import (
+  "net"
+  "bufio"
+  "strings"
+  "errors"
+  "regexp"
+)
+
+type client struct {
+  conn net.Conn
+  in   *bufio.Reader
+  out  *bufio.Writer
+}
+
+func makeClient(c net.Conn) client {
+  return client{ c,
+                 bufio.NewReader(c),
+                 bufio.NewWriter(c) }
+}
+
+// Gets an SMTP command from the client.
+// Verb is the 4-letter SMTP verb.
+// Extra is any characters appearing after the verb (trimmed)
+// On error condition, responds to client with 500 Syntax Error. Never attempts
+// to close the connection.
+func (c *client) getCommand() (verb string, extra string, err error) {
+  line, err := c.readLine()
+  if err != nil {
+    return
+  }
+  err = checkCmdSyntax(line)
+  if err != nil {
+    c.notifySyntaxError()
+    return
+  }
+  verb, extra = splitVerb(line)
+  return
+}
+
+// Reads from the client until the next newline character.
+// Returns the line read after trimming leading/trailing whitespace.
+// On transport error, returns the error.
+func (c *client) readLine() (string, error) {
+  raw, err := c.in.ReadBytes('\n')
+  if err != nil {
+    return "", err
+  }
+  return strings.TrimSpace(string(raw)), nil
+}
+
+func (c *client) notifySyntaxError() {
+  c.out.WriteString("500 Syntax Error\n")
+  c.out.Flush()
+}
+
+// -----------------------------------------------------------------------------
+
+// Checks that a command is formatted exactly "ABCD" or "ABCD EFG XYZ"
+func checkCmdSyntax(s string) error {
+  if !checkCmdSyntaxRegexp.MatchString(s) {
+    return errors.New("Syntax Error")
+  }
+  return nil
+}
+var checkCmdSyntaxRegexp = regexp.MustCompile(`^[A-Z]{4}( .+$|$)`)
+
+// Splits the command into verb and extra parts.
+// Precondition: s is at least 4 characters in length
+func splitVerb(s string) (verb string, extra string) {
+  verb = s[:4]
+  extra = ""
+  if len(s) > 5 {
+    extra = strings.TrimSpace(s[5:])
+  }
+  return
+}
---
+++ smtp_server/client_handler_test.go
@@ -0,0 +1,59 @@
+package smtp_server
+
+import (
+  "testing"
+  "bytes"
+  "bufio"
+  "github.com/stretchr/testify/assert"
+)
+
+func TestSplitVerbLength4(t *testing.T) {
+  verb, extra := splitVerb("ABCD")
+  assert.Equal(t, "ABCD", verb)
+  assert.Equal(t, "", extra)
+}
+
+func TestSplitVerbLength5(t *testing.T) {
+  verb, extra := splitVerb("ABCD ")
+  assert.Equal(t, "ABCD", verb)
+  assert.Equal(t, "", extra)
+}
+
+func TestSplitVerbLength6(t *testing.T) {
+  verb, extra := splitVerb("ABCD E")
+  assert.Equal(t, "ABCD", verb)
+  assert.Equal(t, "E", extra)
+}
+
+func TestSplitVerbLengthExtraSpace(t *testing.T) {
+  verb, extra := splitVerb("ABCD  E")
+  assert.Equal(t, "ABCD", verb)
+  assert.Equal(t, "E", extra)
+}
+
+func TestClientNotifySyntaxError(t *testing.T) {
+  mybuf := new(bytes.Buffer)
+  c := client{nil, bufio.NewReader(new(bytes.Buffer)), bufio.NewWriter(mybuf)}
+  c.notifySyntaxError()
+  assert.Equal(t, "500 Syntax Error\n", mybuf.String())
+}
+
+func TestGetCommand(t *testing.T) {
+  reader := bytes.NewBufferString("MAIL FROM:<foo@bar.com>\n")
+  c := client{nil, bufio.NewReader(reader), bufio.NewWriter(new(bytes.Buffer))}
+  verb, extra, err := c.getCommand()
+  assert.Nil(t, err, "should have no errors")
+  assert.Equal(t, "MAIL", verb)
+  assert.Equal(t, "FROM:<foo@bar.com>", extra)
+}
+
+func TestGetCommandError(t *testing.T) {
+  reader := bytes.NewBufferString("FO\n")
+  writer := new(bytes.Buffer)
+  c := client{nil, bufio.NewReader(reader), bufio.NewWriter(writer)}
+  verb, extra, err := c.getCommand()
+  assert.Equal(t, "", verb, "should have no verb")
+  assert.Equal(t, "", extra, "should have no extra")
+  assert.NotNil(t, err, "should have an error")
+  assert.Equal(t, "500 Syntax Error\n", writer.String())
+}
