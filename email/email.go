a49572f80dd908ceeb728dbf2699f6d9ec0ddeb6
Robert McLaughlin <robert@sparkk.us>
2015-09-07 10:34:35 -0400

    add json serialization to email

--- email/email.go
+++ email/email.go
@@ -11,10 +11,15 @@ type SMTPEmail struct {
 }
 
 func (e *SMTPEmail) ToJson() string {
-  s, _ := json.Marshal(map[string]interface{}{
-    "from": e.From,
-    "to": e.To,
-    "data": e.Data,
-  })
+  s, _ := json.Marshal(e)
   return string(s)
+}
+
+func FromJson(s string) (*SMTPEmail, error) {
+  ret := new(SMTPEmail)
+  err := json.Unmarshal([]byte(s), ret)
+  if err != nil {
+    return nil, err
+  }
+  return ret, nil
 }
--- email/email_test.go
+++ email/email_test.go
@@ -12,9 +12,21 @@ func TestToJson(t *testing.T) {
   e.Data = "whoaimthedata"
   js := e.ToJson()
   assert.Equal( t,
-    `{"data":"whoaimthedata",` +
-    `"from":"foo@bar.com",` +
-    `"to":["rcpt1@foo.com","rcpt2@foo.com"]}`,
+    `{"From":"foo@bar.com",` +
+    `"To":["rcpt1@foo.com","rcpt2@foo.com"],` +
+    `"Data":"whoaimthedata"}`,
     js,
   )
+}
+
+func TestFromJson(t *testing.T) {
+  s := `{"From":"foo@bar.com",` +
+       `"To":["rcpt1@foo.com","rcpt2@foo.com"],` +
+       `"Data":"whoaimthedata"}`
+  got, err := FromJson(s)
+  assert.Nil(t, err, "should have no errors")
+  assert.Equal(t, "foo@bar.com", got.From)
+  assert.Equal(t, "rcpt1@foo.com", got.To[0])
+  assert.Equal(t, "rcpt2@foo.com", got.To[1])
+  assert.Equal(t, "whoaimthedata", got.Data)
 }
