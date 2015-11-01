05f77bdf1c4f72eb8f579a782031e5e1e1980d44
Robert McLaughlin <robert@sparkk.us>
2015-11-01 12:17:20 -0500

    add more explicit test output

--- smtp_server/acceptor_test.go
+++ smtp_server/acceptor_test.go
@@ -11,10 +11,8 @@ import (
 
 func TestGetServerConnection(t *testing.T) {
   l, err := getServerConnection()
-  if err != nil {
-    t.Errorf("Error making server connection")
-    return
-  }
+  assert.NotNil(t, l)
+  assert.NoError(t, err)
   assert.Equal(t, "tcp", l.Addr().Network())
   assert.Equal(t, "127.0.0.1:587", l.Addr().String())
   l.Close()
