0ca9b06250388bbb89ea3449626493ce66ddd447
Robert McLaughlin <rmclaughlin@constantcontact.com>
2015-11-08 16:06:30 -0500

    add test for acceptor shutdown

--- smtp_server/acceptor_test.go
+++ smtp_server/acceptor_test.go
@@ -6,6 +6,7 @@ import (
   "errors"
   "github.com/stretchr/testify/assert"
   "github.com/robmcl4/gost/config"
+  "github.com/robmcl4/gost/config/shutdown"
 )
 
 // -----------------------------------------------------------------------------
@@ -28,17 +29,20 @@ func TestGetServerConnection(t *testing.T) {
 
 type mylistener struct {
   callsToAccept int
+  callsUntilError int
+  closed bool
 }
 
 func (m *mylistener) Accept() (net.Conn, error) {
   m.callsToAccept += 1
-  if (m.callsToAccept >= 2) {
+  if (m.callsToAccept > m.callsUntilError || m.closed) {
     return nil, errors.New("myerror")
   }
   return &net.TCPConn{}, nil
 }
 
 func (m *mylistener) Close() error {
+  m.closed = true
   return nil
 }
 
@@ -48,7 +52,16 @@ func (m *mylistener) Addr() net.Addr {
 
 func TestListenForConnection(t *testing.T) {
   c := make(chan net.Conn, 1)
-  err := listenForConnections(&mylistener{}, c)
+  err := listenForConnections(&mylistener{callsUntilError: 1}, c)
   assert.Equal(t, "myerror", err.Error(), "error message should be \"foo\"")
   <- c
+}
+
+func TestListenForConnectionShutsDown(t *testing.T) {
+  c := make(chan net.Conn, 1)
+  go func() {
+    <- c
+    shutdown.Shutdown()
+  }()
+  listenForConnections(&mylistener{callsUntilError: 6}, c)
 }
